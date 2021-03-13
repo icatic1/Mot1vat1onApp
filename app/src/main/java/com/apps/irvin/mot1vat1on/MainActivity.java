4eb9d95e06ea2063eccd285dddd627e22f2d2191
Irvin Ćatić <icatic1@etf.unsa.ba>
2021-03-13 23:28:37 +0100

    Initial commit

---
+++ app/src/main/java/com/apps/irvin/mot1vat1on/MainActivity.java
@@ -0,0 +1,16 @@
+package com.apps.irvin.mot1vat1on;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int SPLASH_TIME = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
